2318f32fcb9b493cc1308109ce1a01c4e19651ad
sebastianaf <sebastianafanador95@gmail.com>
2022-08-02 02:24:09 -0500

    Updating scripts

---
+++ iot/scripts/current.py
@@ -0,0 +1,34 @@
+
+import os
+import time
+import math
+import ADS1x15
+import dbInsert
+
+analogInput = 1
+factor = 53
+
+ADS = ADS1x15.ADS1015(1)
+ADS.setGain(ADS.PGA_1_024V)
+
+def getRMS(values):
+    i_peak = max(values)
+    return i_peak/(math.sqrt(2))
+
+while True:
+    samplesCounter = 0
+    samplesData = []
+
+    while samplesCounter<20:
+        digitalValue = ADS.readADC(analogInput)
+        voltage = ADS.toVoltage(digitalValue)
+
+        samplesData.append(voltage)
+        samplesCounter += 1
+        time.sleep(1/1000)
+
+    rms = getRMS(samplesData)
+    value = rms*factor
+    insert(value,2)
+    print("%.2f" % (value)) #Amperes
+    time.sleep(1)
---
+++ iot/scripts/dbInsert.py
@@ -0,0 +1,32 @@
+import psycopg2
+from decouple import config
+
+def insert(value,type):
+    print(config("DB_USER"))
+    print(config("DB_PASSWORD"))
+    print(config("DB_DATABASE"))
+    print(config("DB_HOST"))
+    print(config("DB_PORT"))
+    try:
+        connection = psycopg2.connect(user=config("DB_USER"),
+                                    password=config("DB_PASSWORD"),
+                                    host=config("DB_HOST"),
+                                    port=config("DB_PORT"),
+                                    database=config("DB_DATABASE"))
+        cursor = connection.cursor()
+
+        postgres_insert_query = """ INSERT INTO metrics (value, metric_type_id) VALUES (%s,%s)"""
+        record_to_insert = (value,type)
+        cursor.execute(postgres_insert_query, record_to_insert)
+
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "OK")
+
+    except (Exception, psycopg2.Error) as error:
+        print("FAIL", error)
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
