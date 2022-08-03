8ee16a6b318da248910cd75719575ba5305bed9c
sebastianaf <sebastianafanador95@gmail.com>
2022-08-03 16:10:42 -0500

    Update water.py

--- iot/water.py
+++ iot/water.py
@@ -8,22 +8,6 @@ count = 0
 factor = 3.642369
 setEnvironment()
 
-def run():
-    try:
-        start_counter = 1
-        time.sleep(0.5)
-        start_counter = 0
-        global count
-        flow = (count / factor)
-        #print("%.2f" % (flow)) #lts/min
-        count = 0
-        insert(flow,1)
-        time.sleep(0.5)
-    except KeyboardInterrupt:
-        print('\nkeyboard interrupt!')
-        GPIO.cleanup()
-        sys.exit()
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FLOW_SENSOR_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
@@ -33,6 +17,23 @@ def countPulse(channel):
       count = count+1
 
 GPIO.add_event_detect(FLOW_SENSOR_GPIO, GPIO.FALLING, callback=countPulse)
+
+def run():
+    try:
+        start_counter = 1
+        time.sleep(0.5)
+        start_counter = 0
+        global count
+        flow = (count / factor)
+        print("%.2f" % (flow)) #lts/min
+        count = 0
+        insert(flow,1)
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print('\nkeyboard interrupt!')
+        GPIO.cleanup()
+        sys.exit()
+
 
 while True:
     run()
