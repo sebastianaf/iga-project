269264cc579f3ef14cc2ba30b3a6642242ced6f7
sebastianaf <sebastianafanador95@gmail.com>
2022-08-03 15:11:07 -0500

    Recursive run

--- iot/current.py
+++ iot/current.py
@@ -15,20 +15,29 @@ def getRMS(values):
     i_peak = max(values)
     return i_peak/(math.sqrt(2))
 
+def run():
+    try:
+        samplesCounter = 0
+        samplesData = []
+
+        while samplesCounter<20:
+            digitalValue = ADS.readADC(analogInput)
+            voltage = ADS.toVoltage(digitalValue)
+
+            samplesData.append(voltage)
+            samplesCounter += 1
+            time.sleep(1/1000)
+
+        rms = getRMS(samplesData)
+        value = rms*factor
+        insert(value,2)
+        print("%.2f" % (value)) #Amperes
+        time.sleep(1)
+    except():
+        time.sleep(2)
+        run()
+    
+
 while True:
-    samplesCounter = 0
-    samplesData = []
-
-    while samplesCounter<20:
-        digitalValue = ADS.readADC(analogInput)
-        voltage = ADS.toVoltage(digitalValue)
-
-        samplesData.append(voltage)
-        samplesCounter += 1
-        time.sleep(1/1000)
-
-    rms = getRMS(samplesData)
-    value = rms*factor
-    insert(value,2)
-    #print("%.2f" % (value)) #Amperes
-    time.sleep(1)
+    run()
+    
