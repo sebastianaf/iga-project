cb9442f1baa9131aa7698187715bd066c8aac95e
sebastianaf <sebastianafanador95@gmail.com>
2022-08-03 15:41:42 -0500

    Update environment.py

--- iot/environment.py
+++ iot/environment.py
@@ -1,11 +1,9 @@
 import os
 
 def setEnvironment():
-    os.environ["DB_USER"] =
-    os.environ["DB_PASSWORD"] =
-    os.environ["DB_DATABASE"] =
-    os.environ["DB_HOST"] =
-    os.environ["DB_PORT"] =
-    os.environ["TZ"] =
-
-    
+    os.environ["DB_USER"] = ""
+    os.environ["DB_PASSWORD"] = ""
+    os.environ["DB_DATABASE"] = ""
+    os.environ["DB_HOST"] = ""
+    os.environ["DB_PORT"] = ""
+    os.environ["TZ"] = ""
