5353947ffe2f3f80545d0e774ed966f85d22cde4
sebastianaf <sebastianafanador95@gmail.com>
2022-08-03 19:31:33 -0500

    Update dbInsert.py

--- iot/dbInsert.py
+++ iot/dbInsert.py
@@ -2,28 +2,19 @@ import psycopg2
 import os
 
 def insert(value,type):
-    try:
-        connection = psycopg2.connect(
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASSWORD'],
-            host=os.environ['DB_HOST'],
-            port=os.environ['DB_PORT'],
-            database=os.environ['DB_DATABASE'])
-            
-        cursor = connection.cursor()
+    connection = psycopg2.connect(
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        host=os.environ['DB_HOST'],
+        port=os.environ['DB_PORT'],
+        database=os.environ['DB_DATABASE'])
 
-        postgres_insert_query = """ INSERT INTO metrics (value, metric_type_id) VALUES (%s,%s)"""
-        record_to_insert = (value,type)
-        cursor.execute(postgres_insert_query, record_to_insert)
+    cursor = connection.cursor()
 
-        connection.commit()
-        count = cursor.rowcount
-        print("INSERT OK")
+    postgres_insert_query = """ INSERT INTO metrics (value, metric_type_id) VALUES (%s,%s)"""
+    record_to_insert = (value,type)
+    cursor.execute(postgres_insert_query, record_to_insert)
 
-    except:
-        print("INSERT FAIL")
-
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
+    connection.commit()
+    count = cursor.rowcount
+    print("INSERT OK")
